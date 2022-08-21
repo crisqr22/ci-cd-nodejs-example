fa0195688b5623d1ca088b9943b0eb55a064568b
Cristian Quintero <cristianqr22@gmail.com>
2022-08-21 12:11:20 -0500

    add scaffolding

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! deploy';
+    return `Hello World! deploy --> ${process.env.URL_ENV}`;
   }
 }
